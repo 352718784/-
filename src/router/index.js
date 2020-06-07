8466a58fbcdc645b3603d42b76b5d02666d0531e
huangshengqiang <huangshengqiang@kedacom.com>
2020-06-07 11:16:08 +0800

    精简项目

--- src/router/index.js
+++ src/router/index.js
@@ -1,33 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-import kchat from '../components/kchat';
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/kchat',
-      name: 'kcaht',
-      component: kchat
-    },
-    {
-      path: '/helloWorld',
+      path: '/',
       component: HelloWorld
-    },
-    { path: '/hsq',
-      component: { template: '<div>hsq' +
-        '<router-link append to="age">age</router-link>' +
-        '<router-link append to="sex">sex</router-link>' +
-        '<router-view></router-view></div>' },
-      children: [
-        {
-          path: 'age',
-          component: { template: '<div>27</div>', mounted () { console.log(this.$router, this.$route) } }},
-        {
-          path: 'sex', component: {template: '<div>man</div>'}
-        }
-      ]
     }
   ]
 })
